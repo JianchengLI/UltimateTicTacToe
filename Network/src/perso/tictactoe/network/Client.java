37b1f760fe5923129d8d4b1f8cb6a8d742198482
jasonlee <jiancheng.li.2011@gmail.com>
2017-04-11 22:55:49 +0200

    (Network) Hello

--- Network/src/perso/tictactoe/network/Client.java
+++ Network/src/perso/tictactoe/network/Client.java
@@ -1,5 +1,27 @@
 package perso.tictactoe.network;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Client {
+	public static void main(String[] args){
+		try{
+		     Socket socket = new Socket("localhost", 4321);
+		     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+		     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		     
+		     out.println("Hello ~");
+		     
+		   } catch (UnknownHostException e) {
+		     System.out.println("Unknown host: kq6py");
+		     System.exit(1);
+		} catch (IOException e) {
+		     System.out.println("No I/O");
+		     System.exit(1);
+		}
+	}
 }
--- Network/src/perso/tictactoe/network/Server.java
+++ Network/src/perso/tictactoe/network/Server.java
@@ -1,5 +1,40 @@
 package perso.tictactoe.network;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(4321);
+			Socket client = server.accept();
+			try {
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+				 while(true){
+				      try{
+				        String line = in.readLine();
+				        if (line != null) {
+				        	 System.out.println("Text recived : " + line);
+						}
+				       
+				      } catch (IOException e) {
+				        System.out.println("Read failed");
+				        System.exit(-1);
+				      }
+				    }
+			} catch (IOException e) {
+				System.out.println("Read failed");
+				System.exit(-1);
+			}
+		} catch (IOException e) {
+			System.out.println("Could not listen on port 4321");
+			System.exit(-1);
+		}
+
+	}
 }
